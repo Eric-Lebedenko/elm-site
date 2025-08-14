9fbe2c66b36815819290b437ffe5702375908d73
Эрик <45572419+Eric-Lebedenko@users.noreply.github.com>
2025-08-14 15:14:51 +0200

    Delete assets/css/js directory

--- assets/css/js/social-popup.js
+++
@@ -1,6 +0,0 @@
-
-// === Social Popup delayed ===
-(function(){
-  function show(){ const el = document.getElementById("social-popup"); if (el) el.style.display = "block"; }
-  window.addEventListener("load", () => { setTimeout(show, 11000); });
-})();
